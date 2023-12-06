239d5437d1b15b63657aa79f0131d8a0c7a2b72e
Bryan De Jesús Rosa Tavarez <20212239@itla.edu.do>
2023-12-06 19:53:11 -0400

    modification

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,96 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: const Text('Transito RD'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                // <-- SEE HERE
+                decoration:
+                    BoxDecoration(color: Color.fromARGB(255, 50, 54, 84)),
+                accountName: Text(
+                  "Bryan De Jesus Rosa Tavarez",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                accountEmail: Text(
+                  "bryandejesusrt@gmail.com",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                currentAccountPicture: FlutterLogo(),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                ),
+                title: const Text('Inicio'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.payments,
+                ),
+                title: const Text('Tarifario de multas'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.not_listed_location_sharp,
+                ),
+                title: const Text('Consulta de conductor por licencia'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.local_police_outlined,
+                ),
+                title: const Text('Aplicar Multa'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.not_listed_location_sharp,
+                ),
+                title: const Text('Mapa de multas'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.newspaper_rounded,
+                ),
+                title: const Text('Noticias'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.info,
+                ),
+                title: const Text('Acerda de la app'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
         body: const Center(
           child: Text('Hello World'),
