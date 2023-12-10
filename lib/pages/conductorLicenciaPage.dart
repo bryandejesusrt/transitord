130c98ea82f8c751716ca858dfb151a12ced43dd
yencarnacion07 <yoniber.encarnacion@gmail.com>
2023-12-09 23:53:56 -0400

    Tarifario de multas, consultar vehiculo, consultar conductor listos y aplicar multas

--- lib/pages/conductorLicenciaPage.dart
+++ lib/pages/conductorLicenciaPage.dart
@@ -1,21 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:transitord/pages/utils/hexColorsUse.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 import 'package:transitord/widgets/DrawerMenu.dart';
 
-class ConsultarConductorLicencia extends StatelessWidget {
+class ConsultarConductorLicencia extends StatefulWidget {
   static const String routeName = '/ConductorLicencia';
+  @override
+  _MyScreenState createState() => _MyScreenState();
+}
+
+class _MyScreenState extends State<ConsultarConductorLicencia> {
+  TextEditingController licenciaController = TextEditingController();
+  bool _isLoading = false;
+  String mensaje = 'Introduce el numero de licencia.';
+  var data = null;
+
+  Future<void> fetchData() async {
+    print(licenciaController.text);
+    final response = await http.get(Uri.parse(
+        'https://transitord20231207185629.azurewebsites.net/api/v1/Licencia/Licencia/cedula/${licenciaController.text}'));
+    if (response.statusCode == 200) {
+      setState(() {
+        data = json.decode(response.body);
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        mensaje = 'Licencia no encontrada.';
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Consultar Conductor"),
+        title: const Text(
+          "Consulta de licencias de conducir",
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.start,
+        ),
+        backgroundColor: '#359a5c'.toColor(),
+        elevation: 10,
+        shadowColor: Colors.black87,
       ),
       drawer: DrawerMenu(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Titulo con color negro
+            const Text(
+              'Consulta licencias de conducir',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            // Subtitulo con color más opaco
+            const Text(
+              'Ingrese la licencia de conducir a consultar',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(
+              height: 2.h,
+            ),
+            TextFormField(
+              controller: licenciaController,
+              decoration: InputDecoration(
+                hintText: 'Ingrese el numero de licencia',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _isLoading = true;
+                });
+                fetchData();
+              },
+              child: Text('Buscar'),
+            ),
+            SizedBox(
+              height: 1.h,
+            ),
+            _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : data != null
+                    ? Center(
+                        child: Card(
+                          margin: EdgeInsets.all(16.0),
+                          child: Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('nombre: ${data['nombre']}'),
+                                Text('apellido: ${data['apellido']}'),
+                                Text('cedula: ${data['cedula']}'),
+                                Text('foto: ${data['foto']}'),
+                                Text(
+                                    'fecha_Nacimiento: ${data['fecha_Nacimiento']}'),
+                                Text('direccion: ${data['direccion']}'),
+                                Text('telefono: ${data['telefono']}'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text(mensaje),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetalleMultaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalle de la Multa"),
+      ),
       body: Center(
         child: Text(
-          'ConductorLicencia Page',
+          'Detalles de la Multa',
+          style: TextStyle(fontSize: 24),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
--- lib/pages/tarifarioMultasPages.dart
+++ lib/pages/tarifarioMultasPages.dart
@@ -1,21 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:transitord/widgets/DrawerMenu.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class TarifarioMultas extends StatelessWidget {
+class TarifarioMultas extends StatefulWidget {
   static const String routeName = '/tarifario';
+  @override
+  _MyScreenState createState() => _MyScreenState();
+}
+
+class _MyScreenState extends State<TarifarioMultas> {
+  Future<List<Map<String, dynamic>>> fetchData() async {
+    final response = await http.get(Uri.parse(
+        'https://transitord20231207185629.azurewebsites.net/api/v1/MultasTipo/MultasTipos'));
+    if (response.statusCode == 200) {
+      return List<Map<String, dynamic>>.from(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tarifario de Multas"),
+        title: Text('Detalles de Multas'),
       ),
       drawer: DrawerMenu(),
-      body: Center(
-        child: Text(
-          'tarifario Page',
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: fetchData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(
+              child: Text('No se encontraron datos'),
+            );
+          } else {
+            // Muestra la lista de detalles
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final multa = snapshot.data![index];
+                return ListTile(
+                  title: Text('ID: ${multa['id']}'),
+                  subtitle: Text('Descripción: ${multa['descripcion']}'),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
